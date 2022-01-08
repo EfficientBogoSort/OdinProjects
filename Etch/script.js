7e56bc5e35f47c3aa22a482cd4f405f604e2e84d
Pablo Li <76201903+EfficientBogoSort@users.noreply.github.com>
2022-01-08 23:05:46 +1100

    Implemented core logic and created core layout
    Implemented the logic for drawing, as well as the switching betweent
    the different color modes available. Next to do is develop a better
    looking layout and small improvements to the layout such as having a
    "size" text above the size slider for better clarity

--- Etch/script.js
+++ Etch/script.js
@@ -1,7 +1,40 @@
-let board = document.getElementById("board");
+let options = document.getElementById("options");
 let sizeSlider = document.getElementById("slider");
 let sizeIndicator = document.getElementById("gridSize");
+let clearButton = document.getElementById('clear');
+let custColCont = document.getElementById("custColCont");
+let custCol = document.getElementById("custCol");
+let custColButt = document.getElementById("custColButt");
+let randColButt = document.getElementById('randColButt');
+let blackButt = document.getElementById('blackButt');
+let gradButt = document.getElementById('gradButt');
+
+
+
 const BOARD_WIDTH = 480;
+let currColor = "#000";
+let coloring = false;
+let defaultSize = 16;
+
+let mode = {
+    'cust': false,
+    'rand': false,
+    'black': true,
+    'grad': false
+}
+
+custColButt.addEventListener('click', function(){
+    swtichMode('cust');
+});
+randColButt.addEventListener('click', function(){
+    swtichMode('rand');
+});
+blackButt.addEventListener('click', function(){
+    swtichMode('black');
+});
+gradButt.addEventListener('click', function(){
+    swtichMode('grad');
+});
 
 // only readjust the grid once the user has decided on a value
 // this is to avoid creating and deleting grids while the user
@@ -19,6 +52,26 @@ sizeIndicator.oninput = function(){
     resizeSq();
 }
 
+custColCont.style.backgroundColor = custCol.value;
+custCol.addEventListener("input",function(){
+    custColCont.style.backgroundColor = this.value;
+})
+clearButton.addEventListener("click", ()=>{
+    let squares = document.getElementsByClassName('sq');
+    for (let i = 0; i < squares.length; ++i){
+        squares[i].style.backgroundColor = '#FFF';
+    }
+})
+
+function swtichMode(colMode){
+    console.log("A");
+    mode[colMode] = true;
+    for (let key in mode){
+        if (key !== colMode){
+            mode[key] = false;
+        }
+    }
+}
 
 function resizeSq(){
     let squares = document.getElementsByClassName("sq");
@@ -29,19 +82,31 @@ function resizeSq(){
 }
 
 function adjustGrid(){
-    board.appendChild(createGrid(sizeSlider.value));
+    options.insertBefore(createGrid(sizeSlider.value), options.children[1]);
+}
 
+function getRandomColor(){
+    currColor = '#' + Math.floor(Math.random()*16777215).toString(16);
 }
 
 
-let defaultSize = 16;
+function colorSq(sq){
+    sq.style.backgroundColor = currColor;
+}
 
-
-
+function updateCol(){
+    if (mode['cust']){
+        currColor = custCol.value
+    } else if (mode['rand']){
+        getRandomColor();
+    } else if (mode['black']){
+        currColor = '#000';
+    } 
+}
 
 function createGrid(n=16){
-    if (board.childElementCount > 1){
-        board.removeChild(board.lastChild);
+    if (options.childElementCount > 2){
+        options.removeChild(options.children[1]);
     }
     let grid = document.createElement("div");
     grid.id = "grid";
@@ -50,9 +115,24 @@ function createGrid(n=16){
         row.classList.add("row")
         for (let j = 0; j < n;++j){
             let tmp = document.createElement("div");
-            // random color. Temporary, used to allow me to visualize the grid while debugging
-            tmp.style.backgroundColor = '#'+ Math.random().toString(16).slice(-3);
             tmp.classList.add("sq");
+            tmp.addEventListener("mousedown", function(e){
+                coloring = true;
+                updateCol();
+                colorSq(this);
+                e.preventDefault();
+                
+            });
+            tmp.addEventListener("mouseover", function(){
+                if (!coloring){
+                    return;
+                }
+                updateCol();
+                colorSq(this);
+            })
+            tmp.addEventListener("mouseup", ()=>{
+                coloring = false;
+            })
             row.appendChild(tmp);
         }
         grid.appendChild(row);
@@ -70,3 +150,5 @@ window.onload = function(){
     }
     resizeSq();
 }
+
+
